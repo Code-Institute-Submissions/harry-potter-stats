a32da7dac644316233f248d56096b0d29298eaf6
Heinrich Hohbach <heinrich.hohbach@gmail.com>
2018-03-14 17:17:05 +0000

    Connecting Heroku Application to mLab Database.

--- harry_potter_stats.py
+++ harry_potter_stats.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 COLLECTION_NAME = 'charStats'
-DBS_NAME = 'HPStats'
+DBS_NAME = os.environ.get('MONGO_DB_NAME')
+MONGODB_URI = os.environ.get('MONGO_URI')
+
+
 
 
 @app.route("/")
@@ -36,7 +39,7 @@ def donor_projects():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+    with MongoClient(MONGODB_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
